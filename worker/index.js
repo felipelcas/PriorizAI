8d8970d3b2b32f5acf984e91a439f21def5e1e7f
Felipe Castelão <70708333+felipelcas@users.noreply.github.com>
2026-02-23 19:00:13 -0300

    Update index.js

--- worker/index.js
+++ worker/index.js
@@ -34,7 +34,7 @@ export default {
 
       return json({ error: "Rota não encontrada." }, 404);
     } catch (err) {
-      return json({ error: err?.message || "Erro inesperado." }, 1000);
+      return json({ error: err?.message || "Erro inesperado." }, 500);
     }
   },
 };
